4f319e2d30e36296db6f02432a941020f877553a
daverodal <dave.rodal@gmail.com>
2016-01-30 14:31:46 -0500

    Front end now has game map image

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -1,2 +1,34 @@
-asdflklkmk'mwqef
-internal error 500
+<!doctype html>
+<html>
+<style type="text/css">
+    .link-box{
+        background:white;
+        width:50%;
+        margin:250px auto 0;
+        text-align:center;
+        -webkit-border-radius:10px;
+        -moz-border-radius:10px;
+        border-radius: 10px;
+        padding: 30px 0;
+    }
+
+    .link{
+        font-size:40px;
+        color:black;
+
+    }
+    .link:hover{
+        color:red;
+    }
+</style>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/icon">
+</head>
+<body background="{{asset('images/Brandy.png')}}">
+<div class="link-box">
+    <a class="link" href="{{url('wargame/play')}}">GO!</a>
+
+</div>
+</body>
