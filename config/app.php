fe6d97469a57accaa521341c3cd0b8b68394c95f
daverodal <dave.rodal@gmail.com>
2016-02-08 22:08:43 -0500

    added rebellion

--- config/app.php
+++ config/app.php
@@ -158,6 +158,7 @@ return [
         App\Providers\CouchServiceProvider::class,
         App\Providers\BattleProvider::class,
         Wargame\WargameServiceProvider::class,
+        Wargame\Rebellion\RebellionServiceProvider::class,
         Cmgmyr\Messenger\MessengerServiceProvider::class,
 
     ],
