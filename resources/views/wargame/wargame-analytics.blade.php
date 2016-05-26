69bec7f343ee0da9a3ad4340e4e9885130585c40
david <dave.rodal@gmail.com>
2016-05-26 19:48:10 -0400

    Analytics start

--- resources/views/wargame/wargame-analytics.blade.php
+++ resources/views/wargame/wargame-analytics.blade.php
@@ -1,10 +1,46 @@
+<html><head>
+<meta charset="UTF-8">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<script src="{{ asset('js/jquery.js')}}"></script>
+<script src="{{ asset('js/jquery-ui.js')}}"></script>
+<script src="{{ asset('js/sync.js')}}"></script>
+<script src="{{ asset('js/angular.js')}}"></script>
+<link href="{{ asset('css/app.css')}}" rel="stylesheet" type="text/css">
+<style type="text/css">
 
+    body{
+        background: url("{{ asset('images/armoredKnight.jpg')}}") #fff;
+        background-repeat: no-repeat;
+        background-position: center top;
+    }
+    pre{
+        font-size:15px;
+    }
+
+
+
+</style>
+<script type="text/javascript">
+    var DR = {};
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
+    var fetchUrl = '{{ url("wargame/fetch-lobby/") }}';
+</script>
+
+</head>
 @section('content')
     <div id="container" <?= true ? "class='wideGame coolBox'" : 'class="coolBox"'; ?>>
-        <ul id="theGrid">
+        <ul id="analytics-grid">
             @foreach($displayData as $gameName => $gameData)
-                <li class="gridRow">
-                    {{$gameName}} Tie: {{$gameData[0]}} Player One: {{$gameData[1]}} Player Two: {{$gameData[2]}}
+                <li class="grid-row">
+                    <span class="colOne">{{$gameName}}</span>
+                    <span class="colTwo">Tie: {{$gameData[0]}}</span>
+                    <span class="colThree"> Player One: {{$gameData[1]}}</span>
+                    <span class="colFour">Player Two: {{$gameData[2]}}</span>
                 </li>
             @endforeach
 
@@ -14,3 +50,5 @@
         <a href="{{url("/logout")}}">Logout</a>
     </div>
 @show
+
+</html>
