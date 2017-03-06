04814462f9d75fe96dd1c0308dd8103782174557
david_rodal <dave.rodal@gmail.com>
2017-03-06 10:29:09 -0500

    Travis should work now.

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -13,7 +13,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Laravel 5');
+        $this->visit('/')->see('Brandy.png')->see('wargame/play');
+//             ->see('Laravel 5');
     }
 }
