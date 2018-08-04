c9b0c9fe5ae7e620bb76fa84594da6c2a5154530
david <dave.rodal@gmail.com>
2018-08-04 13:37:50 -0400

    made queue's configurable and made playAs use proper html
    which it didn't used to do

--- config/queue.php
+++ config/queue.php
@@ -37,7 +37,7 @@ return [
 
         'database' => [
             'driver' => 'database',
-            'table'  => 'jobs',
+            'table'  => env('QUEUE_TABLE', 'jobs'),
             'queue'  => 'default',
             'expire' => 300,
         ],
