fbb8cf882f9848a948e6ae199130fda9a516581a
david <dave.rodal@gmail.com>
2018-10-26 11:27:47 -0400

    local computer very slow, can't see diff's

--- resources/views/wargame/wargame-scenario-analytics.blade.php
+++ resources/views/wargame/wargame-scenario-analytics.blade.php
@@ -7,9 +7,9 @@
         <ul id="analytics-grid">
             @foreach($displayData as $gameName => $gameData)
                 <li class="row">
-                    <div class="col-xs-5">{{$gameData->key[1]}}</div>
-                    <div class="col-xs-1">{{$gameData->key[2]}}</div>
-                    <div class="col-xs-3">{{date("r",$gameData->key[6])}}</div>
+                    <div class="col-xs-5">{{$gameData->key[2]}}</div>
+                    <div class="col-xs-1">{{$gameData->key[3]}}</div>
+                    <div class="col-xs-3">{{date("r",$gameData->key[7])}}</div>
                     <div class="col-xs-1">{{$gameData->value->winningSide}}</div>
                     <div>
                         <collapsable-box>
