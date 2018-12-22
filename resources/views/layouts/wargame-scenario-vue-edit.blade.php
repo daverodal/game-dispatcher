c4602f667ea7a80e590e5019ef258b6b58451ed7
david <dave.rodal@gmail.com>
2018-12-21 22:03:14 -0500

    added app.js to mix versioning

--- resources/views/layouts/wargame-scenario-vue-edit.blade.php
+++ resources/views/layouts/wargame-scenario-vue-edit.blade.php
@@ -135,6 +135,6 @@
 <footer class="unattached attribution">
     <?=$backgroundAttr;?>
 </footer>
-<script type="text/javascript" src="{{asset('js/app.js')}}"></script>
+<script type="text/javascript" src="{{mix('js/app.js')}}"></script>
 </body>
 </html>
